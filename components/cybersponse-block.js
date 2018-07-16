8195a36fc4857956269991e7d439fd6f316b4390
Nathan Jenan <nathan.jenan@polarity.io>
2018-07-16 07:26:45 -0700

    Making is so a missing incident lead or modify user won't break the template

--- components/cybersponse-block.js
+++ components/cybersponse-block.js
@@ -2,34 +2,39 @@ polarity.export = PolarityComponent.extend({
     details: Ember.computed.alias('block.data.details'),
     actionStatus: {},
     displayResult: Ember.computed('block.data.details', function () {
-        let results = [];
-        let details = this.get('block.data.details');
-        let detail = details.result;
-        let items = [];
-        items.push({ key: 'Description', value: detail.description, hide: true });
-        items.push({ key: 'Impact Assessments', value: detail.impactassessments, hide: true });
-        items.push({ key: 'Phase', value: detail.phase.itemValue });
-        items.push({ key: 'Category', value: detail.category.itemValue });
-        items.push({ key: 'Severity', value: detail.severity.itemValue });
-        items.push({ key: 'Status', value: detail.status.itemValue });
-        items.push({ key: 'Number of Alerts', value: details.numberOfAlerts });
-        if (detail.incidentLead.firstname) {
-            items.push({ key: 'Incident Lead', value: detail.incidentLead.firstname + ' ' + detail.incidentLead.lastname });
-        }
-        items.push({ key: 'Created By', value: detail.createUser.firstname + ' ' + detail.createUser.lastname });
-        if (detail.modifyUser.firstname) {
-            items.push({ key: 'Modified By', value: detail.modifyUser.firstname + ' ' + detail.modifyUser.lastname });
-        }
+        try {
+            let results = [];
+            let details = this.get('block.data.details');
+            let detail = details.result;
+            let items = [];
+            items.push({ key: 'Description', value: detail.description, hide: true });
+            items.push({ key: 'Impact Assessments', value: detail.impactassessments, hide: true });
+            items.push({ key: 'Phase', value: detail.phase.itemValue });
+            items.push({ key: 'Category', value: detail.category.itemValue });
+            items.push({ key: 'Severity', value: detail.severity.itemValue });
+            items.push({ key: 'Status', value: detail.status.itemValue });
+            items.push({ key: 'Number of Alerts', value: details.numberOfAlerts });
+            if (detail.incidentLead && detail.incidentLead.firstname) {
+                items.push({ key: 'Incident Lead', value: detail.incidentLead.firstname + ' ' + detail.incidentLead.lastname });
+            }
+            items.push({ key: 'Created By', value: detail.createUser.firstname + ' ' + detail.createUser.lastname });
+            if (detail.modifyUser && detail.modifyUser.firstname) {
+                items.push({ key: 'Modified By', value: detail.modifyUser.firstname + ' ' + detail.modifyUser.lastname });
+            }
 
-        let match = /^.+\/alerts\/(.+)/.exec(detail['@id'][1]);
-        results.push({
-            name: detail.name,
-            items: items,
-            alert: detail,
-            id: match
-        });
+            let match = /^.+\/alerts\/(.+)/.exec(detail['@id'][1]);
+            results.push({
+                name: detail.name,
+                items: items,
+                alert: detail,
+                id: match
+            });
 
-        return results;
+            return results;
+        } catch (e) {
+            console.error(e);
+            throw e;
+        }
     }),
     host: Ember.computed('block.data.details', function () {
         let details = this.get('block.data.details');
